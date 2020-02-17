9f3e0c2a25385d7f23adea320dacae10fc290d45
smc <cooper.saraah@gmail.com>
2020-02-17 11:30:02 -0500

    user can move carts around using pan gesture

--- MarioKartDemo/ViewController.swift
+++ MarioKartDemo/ViewController.swift
@@ -16,5 +16,20 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func didPanKartView(_ sender: UIPanGestureRecognizer) {
+        //let location = sender.location(in: view)
+        let location = sender.location(in: view)
+        print("Location: x: \(location.x), y: \(location.y)")
+        
+//        Each gesture recognizer knows the view it's attached to. We can ask the gesture recognizer (sender) for it's view in order to access the specific view that was panned (i.e. which kart image view).
+//        💡 We're forcefully unwrapping the view property of the sender using a ! to avoid having to continuously account for it as an optional value. In our case, it's safe to do so since we can be assured that anyone calling this method will have a view attached and not be nil.
+
+        let kartView = sender.view!
+        kartView.center = location
+        
+    }
+    
+    
+    
 }
 
